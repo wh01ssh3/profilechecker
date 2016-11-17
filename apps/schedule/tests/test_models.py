0e162bed6b5d419eccc17d6fdf2cb285267ec561
Anton Prokhorov <betrayer11@gmail.com>
2016-11-17 21:14:52 +0700

    Add 'PeriodicTaskTestCase'

--- apps/schedule/tests/test_models.py
+++ apps/schedule/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
 
+from apps.schedule.factories import PeriodicTaskFactory
 from ..factories import OneTimeTaskFactory
 
 
@@ -13,3 +14,16 @@ class OneTimeTaskTestCase(SimpleTestCase):
         """Test ``__str__`` method"""
         self.assertEqual(str(self.task),
                          '{obj.user} @ {obj.time}'.format(obj=self.task))
+
+
+class PeriodicTaskTestCase(SimpleTestCase):
+    """Test case for ``PeriodicTask``"""
+
+    def setUp(self):
+        self.task = PeriodicTaskFactory()
+
+    def test_str(self):
+        """Ensure that periodic task ``__str__`` method returns ``title``
+        attribute
+        """
+        self.assertEqual(str(self.task), self.task.title)
