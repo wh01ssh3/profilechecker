cbcbeb21fe2a04a14f0ba634796cd29a972ad6aa
Anton Prokhorov <betrayer11@gmail.com>
2016-11-18 22:45:31 +0700

    Add test case for tasks API

---
+++ apps/schedule/tests/test_api.py
@@ -0,0 +1,37 @@
+from rest_framework.test import APITestCase
+
+from apps.schedule.factories import OneTimeTaskFactory, PeriodicTaskFactory
+
+
+class OneTimeTaskViewSetTestCase(APITestCase):
+    """Test case for ``OneTimeTaskViewSet``"""
+
+    def setUp(self):
+        self.task = OneTimeTaskFactory()
+        self.another_task = OneTimeTaskFactory()
+
+        self.client.force_authenticate(self.task.user)
+        self.url = '/api/schedule/one-time-tasks/'
+
+    def test_task_list(self):
+        """Ensure that user can see only his tasks"""
+        resp = self.client.get(self.url)
+        self.assertEqual(len(resp.data), 1)
+        self.assertEqual(resp.data[0]['user'], self.task.user_id)
+
+
+class PeriodicTaskViewSetTestCase(APITestCase):
+    """Test case for ``OneTimeTaskViewSet``"""
+
+    def setUp(self):
+        self.task = PeriodicTaskFactory()
+        self.another_task = PeriodicTaskFactory()
+
+        self.client.force_authenticate(self.task.user)
+        self.url = '/api/schedule/periodic-tasks/'
+
+    def test_task_list(self):
+        """Ensure that user can see only his tasks"""
+        resp = self.client.get(self.url)
+        self.assertEqual(len(resp.data), 1)
+        self.assertEqual(resp.data[0]['user'], self.task.user_id)
