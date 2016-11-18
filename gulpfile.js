cf52aa107e59809c9de052a3bbb92bbbc2052380
Anton Prokhorov <betrayer11@gmail.com>
2016-11-18 20:36:07 +0700

    Set up static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function () {
     // Build css
-    gulp.src('assets/src/css/style.css').pipe(gulp.dest('assets/build/css'));
+    gulp.src('assets/css/style.css').pipe(cleanCSS()).pipe(gulp.dest('static/css'));
 });
 
 gulp.task('default', function () {
