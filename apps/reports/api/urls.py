c922573b5a05e38a18808a44289bfbc35c07495b
Anton Prokhorov <betrayer11@gmail.com>
2016-11-18 23:56:36 +0700

    Permit to create reports for another users's tasks

--- apps/reports/api/urls.py
+++ apps/reports/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OneTimeReportViewSet
 
 router = DefaultRouter()
-router.register('one-time-reposts', OneTimeReportViewSet)
+router.register('one-time-reports', OneTimeReportViewSet,
+                base_name='one_time_reports')
 
 urlpatterns = router.urls
--- apps/reports/api/views.py
+++ apps/reports/api/views.py
@@ -1,11 +1,22 @@
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 
 from .serializers import OneTimeReportSerializer
 from ..models import OneTimeReport
+from ...schedule.models import OneTimeTask
 
 
 class OneTimeReportViewSet(viewsets.ModelViewSet):
     """ViewSet for ``OneTimeReport``"""
 
-    queryset = OneTimeReport.objects.all()
+    def get_queryset(self):
+        return OneTimeReport.objects.filter(task__user=self.request.user)
+
+    def perform_create(self, serializer):
+        task = OneTimeTask.objects.get(id=serializer.data['task'])
+        if task.user != self.request.user:
+            raise ValidationError(
+                _('You can\'t create reports of not your tasks'))
+
     serializer_class = OneTimeReportSerializer
