d5694f4c0a1e94d01df936929bc5eb755f8fd4ee
Anton Prokhorov <betrayer11@gmail.com>
2016-11-19 00:03:12 +0700

    Disallow to update or delete report
    Also disallow to create 2 reports for one time task

--- apps/reports/api/views.py
+++ apps/reports/api/views.py
@@ -1,22 +1,36 @@
 from django.utils.translation import ugettext_lazy as _
-from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework.exceptions import ValidationError
+from rest_framework.viewsets import GenericViewSet
 
 from .serializers import OneTimeReportSerializer
 from ..models import OneTimeReport
 from ...schedule.models import OneTimeTask
 
 
-class OneTimeReportViewSet(viewsets.ModelViewSet):
+class OneTimeReportViewSet(mixins.CreateModelMixin,
+                           mixins.RetrieveModelMixin,
+                           mixins.ListModelMixin,
+                           # Disallow to remove and update
+                           GenericViewSet):
     """ViewSet for ``OneTimeReport``"""
 
     def get_queryset(self):
+        """User should see only his reports"""
         return OneTimeReport.objects.filter(task__user=self.request.user)
 
     def perform_create(self, serializer):
+        """Create report
+
+        User shouldn't be able to create report for task, which is not his
+        task. Also user shouldn't be able to create the second report for task,
+        which already has report
+        """
         task = OneTimeTask.objects.get(id=serializer.data['task'])
         if task.user != self.request.user:
             raise ValidationError(
                 _('You can\'t create reports of not your tasks'))
+        if task.reports.all().exists():
+            raise ValidationError(_('This task already have report'))
 
     serializer_class = OneTimeReportSerializer
