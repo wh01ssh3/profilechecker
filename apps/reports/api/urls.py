49a6f2378c26535ce81155baf8d4e8c3610efd4c
Anton Prokhorov <betrayer11@gmail.com>
2016-11-19 21:13:29 +0700

    Register periodic reports endpoint in urls.py

--- apps/reports/api/urls.py
+++ apps/reports/api/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import OneTimeReportViewSet
+from .views import OneTimeReportViewSet, PeriodicReportViewSet
 
 router = DefaultRouter()
 router.register('one-time-reports', OneTimeReportViewSet,
+                base_name='one_time_reports')
+router.register('periodic-reports', PeriodicReportViewSet,
                 base_name='one_time_reports')
 
 urlpatterns = router.urls
