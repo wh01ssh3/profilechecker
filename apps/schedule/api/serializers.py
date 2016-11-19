4b829bc8fa2896b39751854bf361857d4ecb9f09
Anton Prokhorov <betrayer11@gmail.com>
2016-11-19 22:01:34 +0700

    Change behaviour of 'TaskSerializer'
    Have been added method 'get_rules' which calls 'Task.get_all_rules'
    method, which collects all rules and profiles and removes duplicates,
    also 'TaskSerializer.rules' attribute now is 'SerializerMethodField',
    which bounds with 'TaskSerializer.get_rules'

--- apps/schedule/api/serializers.py
+++ apps/schedule/api/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from ..models import OneTimeTask, PeriodicTask
-from ...rules.api.serializers import ProfileSerializer, RuleSerializer
+from ...rules.api.serializers import RuleSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
     """Base class for task serializers"""
-    profiles = ProfileSerializer(many=True)
-    rules = RuleSerializer(many=True)
+    rules = SerializerMethodField()
+
+    def get_rules(self, obj):
+        """Collect rules from profiles and rules of task"""
+        rules = obj.get_all_rules()
+        serializer = RuleSerializer(instance=rules, many=True)
+        return serializer.data
 
 
 class OneTimeTaskSerializer(TaskSerializer):
