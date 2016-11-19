d6da8b31f3728ebd6d4e43f80a4ff5cc879d44e7
Anton Prokhorov <betrayer11@gmail.com>
2016-11-19 21:31:10 +0700

    Add tests for 'PeriodicReportViewSet'

--- apps/reports/tests/test_api.py
+++ apps/reports/tests/test_api.py
@@ -3,7 +3,8 @@ from rest_framework.test import APITestCase
 
 from ..factories import OneTimeReportFactory
 from ...rules.factories import RuleFactory
-from ...schedule.factories import OneTimeTaskFactory, UserFactory
+from ...schedule.factories import (OneTimeTaskFactory, PeriodicTaskFactory,
+                                   UserFactory)
 
 
 class OneTimeReportViewSetTestCase(APITestCase):
@@ -43,5 +44,40 @@ class OneTimeReportViewSetTestCase(APITestCase):
     def test_user_cant_submit_two_reports(self):
         """Ensure that user can't submit more than one report"""
         OneTimeReportFactory(task=self.task)
+        resp = self.client.post(self.url, self.data)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class PeriodicReportViewSetTestCase(APITestCase):
+    """Test case for ``PeriodicReportViewSet``"""
+
+    def setUp(self):
+        # Create users
+        self.user = UserFactory()
+        self.another_user = UserFactory()
+        # Create task with 2 rules
+        self.task = PeriodicTaskFactory(rules=[RuleFactory(), RuleFactory()],
+                                        user=self.user)
+
+        self.client.force_authenticate(self.user)
+
+        self.url = '/api/reports/periodic-reports/'
+
+        self.data = {
+            'task': self.task.id,
+            'rules': [
+                self.task.rules.all()[0].id
+            ]
+        }
+
+    def test_user_can_submit_report(self):
+        """Test user able to submit a report"""
+        resp = self.client.post(self.url, self.data)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    def test_user_cant_submit_foreign_report(self):
+        """Ensure that user, who's is not task assignee can't submit a report
+        """
+        self.client.force_authenticate(self.another_user)
         resp = self.client.post(self.url, self.data)
         self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
