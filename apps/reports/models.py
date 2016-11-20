d106e423228835e2c902d14732a1060bf936b266
Anton Prokhorov <betrayer11@gmail.com>
2016-11-20 18:19:31 +0700

    Add 'total_rules' attribute to report models

--- apps/reports/models.py
+++ apps/reports/models.py
@@ -3,13 +3,7 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Report(TimeStampedModel):
-    """Base class for reports
-
-    Attributes:
-        passed_rules (list[Rule]): Rules which checking is successful
-    """
-
-    passed_rules = models.ManyToManyField('rules.rule', blank=True)
+    """Base class for reports"""
 
     def __str__(self):
         return '{obj.modified} @ {obj.task}'.format(obj=self)
@@ -19,10 +13,40 @@ class Report(TimeStampedModel):
 
 
 class OneTimeReport(Report):
-    """Model for storing reports for one time tasks"""
+    """Model for storing reports for one time tasks
+
+    Attributes:
+        passed_rules (list[Rule]): Rules which checking is successful
+        total_rules (list[Rule]): Rules which should be checked total
+    """
+    passed_rules = models.ManyToManyField(
+        'rules.rule',
+        blank=True,
+        related_name='one_time_reports_where_passed'
+    )
+    total_rules = models.ManyToManyField(
+        'rules.rule',
+        blank=True,
+        related_name='one_time_reports_where_total'
+    )
     task = models.ForeignKey('schedule.OneTimeTask', related_name='reports')
 
 
 class PeriodicReport(Report):
-    """Model for storing reports for periodic tasks"""
+    """Model for storing reports for periodic tasks
+
+    Attributes:
+        passed_rules (list[Rule]): Rules which checking is successful
+        total_rules (list[Rule]): Rules which should be checked total
+    """
+    passed_rules = models.ManyToManyField(
+        'rules.rule',
+        blank=True,
+        related_name='periodic_reports_where_passed'
+    )
+    total_rules = models.ManyToManyField(
+        'rules.rule',
+        blank=True,
+        related_name='periodic_reports_where_total'
+    )
     task = models.ForeignKey('schedule.PeriodicTask', related_name='reports')
