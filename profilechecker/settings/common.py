c0ed49ebd81773d74815706dd4da7729ccf47132
Anton Prokhorov <betrayer11@gmail.com>
2016-11-20 23:57:02 +0700

    Fix rest framework behaviour

--- profilechecker/settings/common.py
+++ profilechecker/settings/common.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
-    'rest_framework_swagger',
+    'corsheaders',
 
     'apps.rules.apps.RulesConfig',
     'apps.schedule.apps.ScheduleConfig',
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,5 +126,11 @@ STATICFILES_DIRS = (
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
